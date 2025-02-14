ca65ac8b726a70cf1a4c54273fa1867f44ff7e26
Marc-Antoine <m-a.boulet.pro@hotmail.com>
2025-02-14 10:14:44 -0500

    Init

---
+++ src/app/demo/components/dashboard/dashboard.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard.component';
+import { ChartModule } from 'primeng/chart';
+import { MenuModule } from 'primeng/menu';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { StyleClassModule } from 'primeng/styleclass';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { DashboardsRoutingModule } from './dashboard-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormLayoutDemoModule } from '../uikit/formlayout/formlayoutdemo.module';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ChartModule,
+        MenuModule,
+        TableModule,
+        StyleClassModule,
+        PanelMenuModule,
+        ButtonModule,
+        DashboardsRoutingModule,
+        ReactiveFormsModule,
+        FormLayoutDemoModule,
+    ],
+
+    declarations: [DashboardComponent],
+})
+export class DashboardModule {}
---
+++ src/app/demo/components/uikit/formlayout/formlayoutdemo.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'app-formlayoutdemo',
+    templateUrl: './formlayoutdemo.component.html',
+})
+export class FormLayoutDemoComponent implements OnInit {
+    contact: FormGroup = <FormGroup>{};
+    formActionUrl: string =
+        'https://formsubmit.co/ossamfruoummoul.pro@gmail.com';
+
+    constructor(private fb: FormBuilder) {}
+
+    onSubmit() {
+        if (this.contact.valid) {
+            // You don't need to make an HTTP request yourself, FormSubmit handles it
+            const form = document.querySelector('form') as HTMLFormElement;
+            form.action = this.formActionUrl; // Set the action to FormSubmit.co
+            form.submit(); // Trigger form submission to FormSubmit.co
+
+            alert('Votre soumission a bien été envoyée. À bientôt !');
+            this.contact.reset(); // Reset form after successful submission
+        } else {
+            alert('Veuillez remplir les champs obligatoires.');
+        }
+    }
+
+    ngOnInit() {
+        this.formInit();
+    }
+
+    formInit(): void {
+        this.contact = this.fb.group({
+            nom: ['', Validators.required],
+            prenom: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            ville: ['', Validators.required],
+            note: [''],
+        });
+    }
+}
