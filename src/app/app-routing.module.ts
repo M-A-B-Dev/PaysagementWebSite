f67a5ccf1f3d97d8888626920529f9c5be4acf4a
Marc-Antoine <m-a.boulet.pro@hotmail.com>
2025-02-21 10:56:54 -0500

    add contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { AppLayoutComponent } from './layout/app.layout.component';
+import { ContactModule } from './contact/contact.module';
 
 @NgModule({
     imports: [
@@ -18,57 +19,16 @@ import { AppLayoutComponent } from './layout/app.layout.component';
                                     './demo/components/dashboard/dashboard.module'
                                 ).then((m) => m.DashboardModule),
                         },
-                        // {
-                        //     path: 'uikit',
-                        //     loadChildren: () =>
-                        //         import(
-                        //             './demo/components/uikit/uikit.module'
-                        //         ).then((m) => m.UIkitModule),
-                        // },
                         {
-                            path: 'utilities',
+                            path: 'contact',
                             loadChildren: () =>
-                                import(
-                                    './demo/components/utilities/utilities.module'
-                                ).then((m) => m.UtilitiesModule),
-                        },
-                        {
-                            path: 'documentation',
-                            loadChildren: () =>
-                                import(
-                                    './demo/components/documentation/documentation.module'
-                                ).then((m) => m.DocumentationModule),
-                        },
-                        {
-                            path: 'blocks',
-                            loadChildren: () =>
-                                import(
-                                    './demo/components/primeblocks/primeblocks.module'
-                                ).then((m) => m.PrimeBlocksModule),
-                        },
-                        {
-                            path: 'pages',
-                            loadChildren: () =>
-                                import(
-                                    './demo/components/pages/pages.module'
-                                ).then((m) => m.PagesModule),
+                                import('./contact/contact.module').then(
+                                    (m) => m.ContactModule
+                                ),
                         },
                     ],
                 },
-                {
-                    path: 'auth',
-                    loadChildren: () =>
-                        import('./demo/components/auth/auth.module').then(
-                            (m) => m.AuthModule
-                        ),
-                },
-                {
-                    path: 'landing',
-                    loadChildren: () =>
-                        import('./demo/components/landing/landing.module').then(
-                            (m) => m.LandingModule
-                        ),
-                },
+
                 { path: 'notfound', component: NotfoundComponent },
                 { path: '**', redirectTo: '/notfound' },
             ],
