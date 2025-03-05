db9e048b1f0136b830084c13847f9bcd2c485409
Marc-Antoine <m-a.boulet.pro@hotmail.com>
2025-03-05 13:55:22 -0500

    ajust contact form

--- src/app/demo/components/uikit/formlayout/formlayoutdemo.component.ts
+++ src/app/demo/components/uikit/formlayout/formlayoutdemo.component.ts
@@ -9,7 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class FormLayoutDemoComponent implements OnInit {
     contact: FormGroup = <FormGroup>{};
     visible: boolean = false;
-    events: any[] = [];
+
     formActionUrl: string =
         'https://formsubmit.co/ossamfruoummoul.pro@gmail.com';
 
@@ -35,38 +35,6 @@ export class FormLayoutDemoComponent implements OnInit {
 
     ngOnInit() {
         this.formInit();
-        this.events = [
-            {
-                status: 'Demande de soumission : Contactez-nous afin de nous faire part de votre projet.',
-                icon: '1',
-                color: '#ffd54f',
-            },
-            {
-                status: 'Rencontre primaire : Première rencontre afin de prendre connaissance de vos idées et besoins.',
-                icon: '2',
-                color: '#ffd54f',
-            },
-            {
-                status: "Création d’un plan et d’une soumission: Selon l’ampleur du projet, un membre de notre équipe ou un designer paysagiste s'affaire à concevoir un plan personnalisé ainsi qu’une soumission détaillée du projet.",
-                icon: '3',
-                color: '#ffd54f',
-            },
-            {
-                status: 'Rencontre secondaire : Deuxième rencontre ayant pour but de bien vous expliquez le projet et répondre à toutes vos interrogations.',
-                icon: '4',
-                color: '#ffd54f',
-            },
-            {
-                status: 'Acceptation finale : Dernière étape avant la création, nous signons le contrat de travail et statuons sur la date de début des travaux.',
-                icon: '5',
-                color: '#ffd54f',
-            },
-            {
-                status: 'Réalisation du projet : L’étape tant attendue arrive enfin, notre équipe dévoué procédera à la conception de votre espace unique de qualité/durable/esthétique',
-                icon: '6',
-                color: '#ffd54f',
-            },
-        ];
     }
 
     formInit(): void {
@@ -75,6 +43,7 @@ export class FormLayoutDemoComponent implements OnInit {
             prenom: ['', Validators.required],
             email: ['', [Validators.required, Validators.email]],
             ville: ['', Validators.required],
+            phone: ['', Validators.required],
             note: [''],
         });
     }
