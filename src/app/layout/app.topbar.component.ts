fd9fd09d28f11f7fa03fbd37228e7f0606b9456b
Marc-Antoine <m-a.boulet.pro@hotmail.com>
2025-03-05 13:15:23 -0500

    Mobil menu is working

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -1,5 +1,4 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { MenuItem } from 'primeng/api';
 import { LayoutService } from './service/app.layout.service';
 
 @Component({
@@ -7,21 +6,26 @@ import { LayoutService } from './service/app.layout.service';
     templateUrl: './app.topbar.component.html',
 })
 export class AppTopBarComponent {
-    items!: MenuItem[];
+    sidebarVisible: boolean = false;
 
     @ViewChild('menubutton') menuButton!: ElementRef;
-
     @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
-
     @ViewChild('topbarmenu') menu!: ElementRef;
 
     constructor(public layoutService: LayoutService) {}
-    sidebarVisible: boolean = false;
 
     // Fonction pour basculer la visibilité du menu latéral
     toggleSidebar() {
         this.sidebarVisible = !this.sidebarVisible;
     }
+
+    // Nouvelle fonction pour fermer le sidebar avec un léger délai après le clic
+    closeSidebarWithDelay() {
+        setTimeout(() => {
+            this.sidebarVisible = false; // Ferme le sidebar après un délai
+        }, 200); // Délai de 200ms pour permettre la navigation
+    }
+
     configButton() {
         this.layoutService.showConfigSidebar();
     }
